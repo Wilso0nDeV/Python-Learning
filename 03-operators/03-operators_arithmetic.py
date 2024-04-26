2de5e519d3f4f07e05dd71bfdda0a9dc5a3da639
wilsonvasquez <ewilsonvc.dev@gmail.com>
2024-04-25 20:03:51 -0500

    exercise

---
+++ 03-operators/03-operators_arithmetic.py
@@ -0,0 +1,34 @@
+#Operatos in Python
+
+#Addition (+) : do you can sum numbers int or float
+
+print(1+2) #3
+print(1.0 + 2.3) #3.3
+
+#Substraction (-) 
+print(2-1) #1
+print(1-2) #-1
+print(1.0 - 2.3) #-1.2999999999999998
+
+#Multiplication(*)
+print(2*2) #4
+print(-4 * -5) #20
+print(2.3 * 3.5) 
+
+#Modules(%) : Get the residue of a division
+print(2%3) #2
+print(10%2) #0
+
+#Division(/)
+print(10/2) #5
+#print(3/0) #Error
+
+#Exponential(**)
+print(2**3) #8
+print(123123124151213123**0) #1
+
+#Floor division operator(//) :  this omits decimals
+print(3//2) #1
+
+print(10%5) #0
+
---
+++ 03-operators/exercise.py
@@ -0,0 +1,6 @@
+
+
+#Find an Euclidian distance between (2, 3) and (10, 8)
+# p1: 2, #p2 : 3 , q1:10, q2:8
+#formula :  (((p1-q1)**2) + ((p2 - q2)**2)) ** 0.5
+print( (((2 - 10) ** 2) + ((3 - 8) ** 2)) ** 0.5  )
